d6ce189e452239df1cfdd9196f3ca45fbca1f2f4
blackiso <ismailboy00@gmail.com>
2019-02-12 17:50:54 +0100

    updated users

--- modules/users.php
+++ modules/users.php
@@ -1,10 +1,19 @@
 <?php
 	/**
-	 * 
+	 * This class contains all the methods to handlle users requests
+	 * @param Method : String
+	 * @param Id : String | Null
+	 * @param Sub collection : String | Null
+	 * @param Params : Array | Null
+	 * @return Object
 	 */
-	class users {
-		
-		function __construct() {
-			echo "string";
+	class users extends Module {
+		// Class constructor
+		function __construct($method, $id = null, $sub_collection = null, $params = null) {
+			parent::__construct($method);
 		}
+
+    	protected function post() {
+    		echo "post";
+    	}
 	}
