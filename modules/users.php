5a18a4e42a5f6d6afaea00ee03697a719a034a95
blackiso <ismailboy00@gmail.com>
2019-02-13 13:02:43 +0100

    updated users module

--- modules/users.php
+++ modules/users.php
@@ -9,25 +9,63 @@
 	 */
 	class users extends Module {
 		// Setting parameters
-		protected $id;
+		protected $user_ID;
+		protected $room_ID;
+		protected $username;
 		protected $sub_collection;
 		protected $params;
 
 		// Class constructor
-		function __construct($method, $id = null, $sub_collection = null, $params = null) {
-			parent::__construct($method);
-			$this->id = $id;
+		function __construct($method, $user_ID = null, $sub_collection = null, $params = null) {
+			// Setup all parameters
+			$this->user_ID = $user_ID;
 			$this->sub_collection = $sub_collection;
 			$this->params = $params;
+
+			// Get data form request body
+			$request_data = $this->get_request_body();
+			$this->username = isset($request_data->username) ? htmlentities($request_data->username) : null;
+			$this->room_ID = $request_data->room_ID;
+
+			// Run parent constructor
+			parent::__construct($method);
 		}
-		// Join a chat room
+		/**
+    	* This method is for handling POST requests for users class
+    	* @return Array 
+    	*/
     	protected function post() {
-    		// Create a user
-    		if ($this->id = null) $this->id = "ID".uniqid();
-    		$user_query = $this->db->query("")
+    		// Add user to room
+    		// Generate a new ID for user
+    		if ($this->user_ID == null) $this->user_ID = "ID".uniqid();
+    		$user_query = $this->db->query("INSERT INTO users (user_ID, room_ID, username, session_ID)
+    						VALUES ('$this->user_ID', '$this->room_ID', '$this->username', '".SESSID."')", false);
+    		if ($user_query) {
+    			$result = array();
+    			$result['user_ID']    = $this->user_ID;
+    			$result['room_ID']    = $this->room_ID;
+    			$result['username']   = $this->username;
+    			$result['session_ID'] = SESSID;
+
+    			return $result;
+    		}else {
+    			$this->write_error("User join error!");
+    		}
     	}
-    	// Change / add options
+    	/**
+    	* This method is for handling PUT requests for users class
+    	* @return Array 
+    	*/
     	protected function put() {
     		
+    	}
+    	/**
+    	* This method is for changing user option in database
+    	* @param option : String
+    	* @param value  : Boolean
+    	* @return Null
+    	*/
+    	private function set_option($option, $value) {
+
     	}
 	}
