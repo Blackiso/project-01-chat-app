3a91f65c0661f90382bb01d723c6fdf1ab46228f
blackiso <ismailboy00@gmail.com>
2019-02-13 17:03:14 +0100

    _

--- modules/rooms.php
+++ modules/rooms.php
@@ -8,29 +8,29 @@
 	 * @return Object
 	 */
 	class rooms extends Module {
-		// Setting parameters
+        // Setting parameters
         protected $room_ID;
         protected $session_ID = SESSID;
-		protected $room_name;
-		protected $sub_collection;
+        protected $room_name;
+        protected $sub_collection;
         protected $params;
-		protected $options;
+        protected $options;
 
-		// Class constructor
-		function __construct($method, $room_ID = null, $sub_collection = null, $params = null) {
-			// Setup all parameters
-			$this->room_ID = $room_ID !== null ? $room_ID : "ID".uniqid();
+        // Class constructor
+        function __construct($method, $room_ID = null, $sub_collection = null, $params = null) {
+        	// Setup all parameters
+        	$this->room_ID = $room_ID !== null ? $room_ID : "ID".uniqid();
             $this->sub_collection = $sub_collection;
             $this->params = $params;
 
-			// Run parent constructor
-			parent::__construct($method);
-		}
-		/**
-    	* This method is for handling POST requests for users class
-    	* @return Array 
-    	*/
-    	protected function post() {
+        	// Run parent constructor
+        	parent::__construct($method);
+        }
+        /**
+        * This method is for handling POST requests for users class
+        * @return Array 
+        */
+        protected function post() {
             // Get data form request body
             $request_data = $this->get_request_body();
             $this->room_name = htmlentities($request_data->room_name);
@@ -55,23 +55,23 @@
             }else {
                 $this->write_error("Room name already exist!");
             }
-    	}
-    	/**
-    	* This method is for handling PUT requests for users class
-    	* @return Array 
-    	*/
-    	protected function put() {
-    		
-    	}
-    	/**
-    	* This method is for changing user option in database
-    	* @param option : String
-    	* @param value  : Boolean
-    	* @return Null
-    	*/
-    	private function set_option($option, $value) {
+        }
+        /**
+        * This method is for handling PUT requests for users class
+        * @return Array 
+        */
+        protected function put() {
+        	
+        }
+        /**
+        * This method is for changing user option in database
+        * @param option : String
+        * @param value  : Boolean
+        * @return Null
+        */
+        private function set_option($option, $value) {
 
-    	}
+        }
 	}
 
 
